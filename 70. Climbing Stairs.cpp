fa77f029894de1f351fb045e3ace0df816c5f354
shristibansal <shristibansal6@gmail.com>
2022-06-08 12:57:20 +0530

    code

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+      int a = 1;
+      int b = 1;
+
+      for(int i = 2; i <= n; i++){
+          int c = a + b;
+          b = a;
+          a = c;
+      }
+        return a;  
+    }
+};
